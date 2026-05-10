1437ca112f72189fa86da3cce0d08695a73fb6aa
prasadmv-collab <prasadmvmsdpsd@gmail.com>
2026-05-10 22:11:42 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,21 +3,31 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        WebDriver driver=new ChromeDriver();
+        //  Add this block
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");   // required for Jenkins
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        //  Replace driver creation
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        
+        //  maximize may fail in headless, so optional
+        // driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        // good practice
+        driver.quit();
     }
 }
-
